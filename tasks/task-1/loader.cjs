a7f4c8f56651851b17c0d3b76251e4c087552368
Maksim Dubovskii <Maksim.Dubovskiy@odysmtech.ru>
2025-12-05 14:45:11 +0300

    Create yml loaders

--- tasks/task-1/loader.cjs
+++ tasks/task-1/loader.cjs
@@ -1,2 +1,10 @@
+const fs = require("fs");
 const yaml = require("yaml");
-const fs = require("node:fs");
+
+require.extensions[".yml"] = function (module, filename) {
+    const content = fs.readFileSync(filename, "utf8");
+    const parsed = yaml.parse(content);
+    module.exports = parsed;
+};
+
+require.extensions[".yaml"] = require.extensions[".yml"];
--- tasks/task-1/loader.mjs
+++ tasks/task-1/loader.mjs
@@ -1,3 +1,19 @@
-import fs from "node:fs/promises";
-import { fileURLToPath } from "node:url";
+import fs from "fs/promises";
 import yaml from "yaml";
+import { fileURLToPath } from "node:url";
+
+export async function load(url, context, nextLoad) {
+    if (url.endsWith(".yml") || url.endsWith(".yaml")) {
+        const filepath = fileURLToPath(url);
+        const source = await fs.readFile(filepath, "utf8");
+        const parsed = yaml.parse(source);
+
+        return {
+            format: "module",
+            source: `export default ${JSON.stringify(parsed)};`,
+            shortCircuit: true
+        };
+    }
+
+    return nextLoad(url, context);
+}
