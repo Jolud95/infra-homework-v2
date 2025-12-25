a284296cb993146865bbf5a5870cddd83ac13bf4
Maksim Dubovskii <Maksim.Dubovskiy@odysmtech.ru>
2025-12-25 16:53:36 +0300

    HW-7: Write commit-msg git hook

--- tasks/task-7/lint-commit.js
+++ tasks/task-7/lint-commit.js
@@ -1,8 +1,26 @@
 import fs from "node:fs";
+import path from 'path';
 
 const TYPES = ["feat", "fix", "chore", "docs", "refactor", "test", "style"];
+const MERGE = 'Merge';
 
 // Read file with commit message
-const args = process.argv.slice(2);
+const commitMsgFilePath = process.argv[2];
+const commitMsg = fs.readFileSync(path.resolve(commitMsgFilePath), 'utf8');
 
 // Validate commit message
+const isTypeMsgCorrect = TYPES.some((type) => commitMsg.startsWith(`${type}: `));
+const isMergeMsg = commitMsg.startsWith(`${MERGE} `);
+
+if (isTypeMsgCorrect) {
+    console.log('VALID COMMIT');
+    process.exit(0);
+}
+if (isMergeMsg) {
+    console.log('VALID MERGE');
+    process.exit(0);
+}
+
+process.exit(1);
+
+
