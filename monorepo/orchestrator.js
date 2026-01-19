d8a74ad3347f3e8798cee2d89e80ae376d8a8e24
Maksim Dubovskii <Maksim.Dubovskiy@odysmtech.ru>
2026-01-19 21:15:10 +0300

    HW-10: Create monorepo orchestrator

--- monorepo/orchestrator.js
+++ monorepo/orchestrator.js
@@ -2,16 +2,89 @@ import fs from "node:fs";
 import path from "node:path";
 import { exec } from "node:child_process";
 
-function findProjects() {}
+const ROOT_DIR = process.cwd();
+const SCRIPT_NAME = 'build';
 
-function runScript(project) {}
+/**
+ * Находим проекты из workspaces
+ */
+function findProjects() {
+    const rootPackageJson = JSON.parse(
+        fs.readFileSync(path.join(ROOT_DIR, "package.json"), "utf-8")
+    );
 
-async function run() {
-  const projects = findProjects();
+    const workspaces = rootPackageJson.workspaces || [];
+    let projects = [];
 
-  for (const project of projects) {
-    await runScript(project);
-  }
+    for (const workspace of workspaces) {
+        const baseDir = workspace.replace("/**", "");
+        const absBaseDir = path.join(ROOT_DIR, baseDir);
+
+        if (!fs.existsSync(absBaseDir)) continue;
+        projects = [...projects, ...findPackageJson(absBaseDir)]
+    }
+
+    return projects;
 }
 
-run();
+function findPackageJson(curDir, projects = []) {
+    let newProjects = [...projects];
+    const dirs = (fs.readdirSync(curDir, { withFileTypes: true })).filter((dir) => dir.isDirectory());
+
+    if (!dirs.length) return newProjects;
+
+    for (const dir of dirs) {
+        const projectDir = path.join(curDir, dir.name);
+        const packageJsonPath = path.join(projectDir, "package.json");
+
+        if (fs.existsSync(packageJsonPath)) {
+            newProjects = [...newProjects, {
+                name: dir.name,
+                dir: projectDir,
+                packageJson: JSON.parse(
+                    fs.readFileSync(packageJsonPath, "utf-8")
+                ),
+            }];
+        } else {
+            newProjects = findPackageJson(projectDir, newProjects)
+        }
+    }
+
+    return newProjects;
+}
+
+/**
+ * Запускаем build или пропускаем
+ */
+function runScript(project) {
+    return new Promise((resolve, reject) => {
+        if (!project.packageJson.scripts[SCRIPT_NAME]) {
+            const line = `[${project.name}] skipped (no ${SCRIPT_NAME})`;
+            resolve(line);
+            return;
+        }
+
+        exec(`npm run ${SCRIPT_NAME}`, { cwd: project.dir }, (error) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            const line = `[${project.name}] ${SCRIPT_NAME} finished`;
+            resolve(line);
+        });
+    });
+}
+
+async function run() {
+    const projects = findProjects();
+    const results = [];
+
+    for (const project of projects) {
+        const result = await runScript(project);
+        results.push(result)
+    }
+    debugger;
+    return results;
+}
+const results = await run();
+console.log(results.join("\n"));
